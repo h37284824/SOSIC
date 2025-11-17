e2200a5832b45449cd61ec0986869faf64b0b245
정원욱 <__jungww6085@gmail.com>
2025-11-17 17:21:12 +0900

    fear: 라우터 설치 원욱

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Login from "./Login.jsx";
 import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>      
       <div className="App">
-        <Login />
+        <Route>
+          <Route path="/" element= {<Login />}></Route>
+
+          <Route path="/signup" element= {<Signup />}></Route>
+        </Route>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
